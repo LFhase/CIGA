7fbed8a40ea922b994cf9dceea5eb4e00f41edda
Andrew Chan <rimemosa@163.com>
2023-08-29 00:05:47 +0800

    Update mask.py for PyG 2.0.4

--- utils/mask.py
+++ utils/mask.py
@@ -7,11 +7,19 @@ from torch_geometric.nn.conv import MessagePassing
 def set_masks(mask: Tensor, model: nn.Module):
         for module in model.modules():
             if isinstance(module, MessagePassing):
+                #PyG 2.0.4
+                module._explain = True
+                module._edge_mask = mask
+                #PyG 1.7.2
                 module.__explain__ = True
                 module.__edge_mask__ = mask
 
 def clear_masks(model: nn.Module):
     for module in model.modules():
         if isinstance(module, MessagePassing):
+            #PyG 2.0.4
+            module._explain = False
+            module._edge_mask = None
+            #PyG 1.7.2
             module.__explain__ = False
             module.__edge_mask__ = None
