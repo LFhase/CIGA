454801108737ff8855ac2be947201dd9338dff37
LFhase <rimemosa@163.com>
2023-08-29 00:07:45 +0800

    Update mask.py for PyG 2.0.4

--- utils/mask.py
+++ utils/mask.py
@@ -5,14 +5,14 @@ from torch_geometric.typing import OptTensor
 from torch_geometric.nn.conv import MessagePassing
 
 def set_masks(mask: Tensor, model: nn.Module):
-        for module in model.modules():
-            if isinstance(module, MessagePassing):
-                #PyG 2.0.4
-                module._explain = True
-                module._edge_mask = mask
-                #PyG 1.7.2
-                module.__explain__ = True
-                module.__edge_mask__ = mask
+    for module in model.modules():
+        if isinstance(module, MessagePassing):
+            #PyG 2.0.4
+            module._explain = True
+            module._edge_mask = mask
+            #PyG 1.7.2
+            module.__explain__ = True
+            module.__edge_mask__ = mask
 
 def clear_masks(model: nn.Module):
     for module in model.modules():
